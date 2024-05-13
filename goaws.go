9276f5bb54ae29f1a8b2f8e3a9e3c16f1be62e6e
Oliver Säfström <safstrom.oliver@gmail.com>
2024-05-13 11:57:52 +0200

    Add dymnamodb and connect lambda

--- goaws.go
+++ goaws.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -19,11 +20,21 @@ func NewGoawsStack(scope constructs.Construct, id string, props *GoawsStackProps
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	awslambda.NewFunction(stack, jsii.String("golambda"), &awslambda.FunctionProps{
+	table := awsdynamodb.NewTable(stack, jsii.String("goUserTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		TableName: jsii.String("userTable"),
+	})
+
+	myFunction := awslambda.NewFunction(stack, jsii.String("golambda"), &awslambda.FunctionProps{
 		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
 		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
 		Handler: jsii.String("main"),
 	})
+	// Connect lambda with db
+	table.GrantReadWriteData(myFunction)
 
 	// The code that defines your stack goes here
 
