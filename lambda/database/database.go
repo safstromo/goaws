3ac625e3ac4b15dc91b55d994aa9871468cb8b49
Oliver Säfström <safstrom.oliver@gmail.com>
2024-05-13 11:40:34 +0200

    Add insert user logic

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,8 +1,15 @@
 package database
 
 import (
+	"lamda-func/types"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+const (
+	TABLE_NAME = "usersTable"
 )
 
 type DynamoDBClient struct {
@@ -17,4 +24,47 @@ func NewDynamoDBClient() DynamoDBClient {
 	return DynamoDBClient{
 		databaseStore: db,
 	}
+}
+
+func (u DynamoDBClient) DoesUserExist(username string) (bool, error) {
+	result, err := u.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(username),
+			},
+		},
+	})
+	if err != nil {
+		return true, err
+	}
+
+	if result.Item == nil {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (u DynamoDBClient) InsertUser(user types.RegisterUser) err {
+	// assemble item
+	// TODO: encrypt
+	item := &dynamodb.PutItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Item: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(user.Username),
+			},
+			"password": {
+				S: aws.String(user.Password),
+			},
+		},
+	}
+
+	_, err := u.databaseStore.PutItem(item)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
